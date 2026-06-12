78edb720541cb589dbb33fcd89c5261ae4589f35
bluejay-19 <emlinbino1218@gmail.com>
2026-06-12 16:50:04 +0200

    Rename order of app pages

--- pages/3_Upload.py
+++ pages/3_Upload.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import load_css, render_sidebar
+from utils import load_css, render_sidebar, check_auth
 
 st.set_page_config(
     page_title="Vault — Upload",
@@ -8,8 +8,12 @@ st.set_page_config(
 )
 
 load_css()
+check_auth()
 render_sidebar(active_page="upload")
 
 st.markdown("## Upload Statements")
 st.markdown("<p style='color: #A0AEC0;'>CSV or Excel files. Frank will do the rest.</p>", unsafe_allow_html=True)
-st.info("🚧 Upload — coming Day 3")
+
+
+# Single sheet = single month 
+# TODO: Expand it to multi sheet parsing where the isngle excel upload has multiple sheets covering muultiple montths 
