0780541cad8b7561eb4dd4c9c9c72674cc79fdbf
bluejay-19 <emlinbino1218@gmail.com>
2026-06-12 22:01:05 +0200

    Adds file uploader, income, savings & budget buttons

--- pages/3_Upload.py
+++ pages/3_Upload.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd 
 from utils import load_css, render_sidebar, check_auth
 
 st.set_page_config(
@@ -14,6 +15,17 @@ render_sidebar(active_page="upload")
 st.markdown("## Upload Statements")
 st.markdown("<p style='color: #A0AEC0;'>CSV or Excel files. Frank will do the rest.</p>", unsafe_allow_html=True)
 
+# File uploader 
+uploaded_file = st.file_uploader(
+    "Upload statement",
+    type=["csv", "xlsx"]
+)
 
-# Single sheet = single month 
-# TODO: Expand it to multi sheet parsing where the isngle excel upload has multiple sheets covering muultiple montths 
+# Income this month 
+income = st.number_input("Income this month", min_value=0, value=0, step=100)
+# Budget this month 
+budget = st.number_input("Budget this month", min_value=0, value=0, step=100)
+# Savings this month 
+savings = st.number_input("Net Savings", value=0, step=100)
+
+# Parsing the uploaded file
