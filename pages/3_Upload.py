f81e486cf1982eece9a873ca8eb8c6fcc2704cca
bluejay-19 <emlinbino1218@gmail.com>
2026-06-12 22:01:50 +0200

    Add file parsing and column validation

--- pages/3_Upload.py
+++ pages/3_Upload.py
@@ -29,3 +29,36 @@ budget = st.number_input("Budget this month", min_value=0, value=0, step=100)
 savings = st.number_input("Net Savings", value=0, step=100)
 
 # Parsing the uploaded file
+
+if uploaded_file is not None: 
+    try: 
+        if uploaded_file.name.endswith(".csv"):
+            df = pd.read_csv(uploaded_file)
+            st.write("File looks good enough")
+        else: 
+            df = pd.read_excel(uploaded_file)
+            st.write("File looks good enough")
+    except: 
+        st.error("So, whatever you just added did not work, I cannot read it, how about making it something I can read.")
+        st.stop()
+
+    # Validate the columns
+    
+    # Is df empty?
+    if df.empty: 
+        st.error("Yo! what do you expect me to do with a blank file, want me to draw you a picture? ")
+        st.stop()
+
+    # Does Date column exist 
+    if 'Date' not in df.columns:
+        st.error("How do you expect to analyse your spending without knowing when you spent the money. Where are the dates buddy?")
+        st.stop()
+
+    # Do at least one of the following exist: food, rent, transport, entertainment, other exist 
+    required_cols = {'Food', 'Rent', 'Transport', 'Entertainment', 'Other', 'Subscriptions', 'Groceries', 'Medical'}
+    if df.columns.isin(required_cols).any():
+        st.write("okay cool nice categories")
+    else: 
+        st.error("Okay, so no categories, you couldnt do column names? really!")
+        st.stop()
+    
